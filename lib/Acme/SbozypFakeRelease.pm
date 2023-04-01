1c2887c996dea194f584f0cee98fbf41a5f4fc94
Nicholas Hubbard <nicholashubbard@posteo.net>
2023-04-01 10:52:55 -0400

    fake software release to be used in test code

---
+++ lib/Acme/SbozypFakeRelease.pm
@@ -0,0 +1,14 @@
+package Acme::SbozypFakeRelease;
+
+1;
+
+__END__
+
+=pod
+
+=head1 SbozypFakeRelease
+
+This is a fake module used for creating a fake distribution to be used by
+sbozyp test code.
+
+=cut
