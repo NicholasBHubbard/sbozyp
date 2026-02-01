f8808b6c5f7580604cf17b6758b8b944aa155173
Nicholas B. Hubbard <nicholashubbard@posteo.net>
2026-02-01 13:05:01 -0500

    minor: documentation

--- lib/App/sbozyp.pm
+++ lib/App/sbozyp.pm
@@ -2,7 +2,8 @@ package App::sbozyp;
 
 =pod
 
-This is a sham module that exists to get CPAN to index the distribution.
+This is a sham module that exists to get CPAN to index the distribution. This
+module is not installed onto a users system in normal circumstances.
 
 =cut
 
